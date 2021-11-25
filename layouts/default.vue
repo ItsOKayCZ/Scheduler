bf760ea8104afc02cc5ee28bea70bc72c9bb8c28
ItsOKayCZ <DjSlimeLP@gmail.com>
2021-11-25 13:56:36 +0100

    Added toolbar

--- layouts/default.vue
+++ layouts/default.vue
@@ -4,74 +4,14 @@
 			<nuxt-child :navigation='navigation' />
 		</v-main>
 
-		<v-bottom-navigation fixed>
+
+		<v-bottom-navigation app :height='bottomNavigationHeight' grow>
 			<v-btn v-for='(item, index) in navigation' :key='index' :to='item.to'>
 				<span>{{ item.title }}</span>
 				<v-icon>{{ item.icon }}</v-icon>
 			</v-btn>
 		</v-bottom-navigation>
 	</v-app>
-  <!-- <v-app dark>
-    <v-navigation-drawer
-      v-model="drawer"
-      :mini-variant="miniVariant"
-      :clipped="clipped"
-      fixed
-      app
-    >
-      <v-list>
-        <v-list-item
-          v-for="(item, i) in items"
-          :key="i"
-          :to="item.to"
-          router
-          exact
-        >
-          <v-list-item-action>
-            <v-icon>{{ item.icon }}</v-icon>
-          </v-list-item-action>
-          <v-list-item-content>
-            <v-list-item-title v-text="item.title" />
-          </v-list-item-content>
-        </v-list-item>
-      </v-list>
-    </v-navigation-drawer>
-    <v-app-bar :clipped-left="clipped" fixed app>
-      <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
-      <v-btn icon @click.stop="miniVariant = !miniVariant">
-        <v-icon>mdi-{{ `chevron-${miniVariant ? "right" : "left"}` }}</v-icon>
-      </v-btn>
-      <v-btn icon @click.stop="clipped = !clipped">
-        <v-icon>mdi-application</v-icon>
-      </v-btn>
-      <v-btn icon @click.stop="fixed = !fixed">
-        <v-icon>mdi-minus</v-icon>
-      </v-btn>
-      <v-toolbar-title v-text="title" />
-      <v-spacer />
-      <v-btn icon @click.stop="rightDrawer = !rightDrawer">
-        <v-icon>mdi-menu</v-icon>
-      </v-btn>
-    </v-app-bar>
-    <v-main>
-      <v-container>
-        <Nuxt />
-      </v-container>
-    </v-main>
-    <v-navigation-drawer v-model="rightDrawer" :right="right" temporary fixed>
-      <v-list>
-        <v-list-item @click.native="right = !right">
-          <v-list-item-action>
-            <v-icon light> mdi-repeat </v-icon>
-          </v-list-item-action>
-          <v-list-item-title>Switch drawer (click me)</v-list-item-title>
-        </v-list-item>
-      </v-list>
-    </v-navigation-drawer>
-    <v-footer :absolute="!fixed" app>
-      <span>&copy; {{ new Date().getFullYear() }}</span>
-    </v-footer>
-  </v-app> -->
 </template>
 
 <script>
@@ -95,13 +35,29 @@ export default{
 					to: '/stats'
 				}
 			],
-		}
+			bottomNavigationHeight: 50
+		};
 	},
+	computed: {
+		fullPageWithoutBottomNavigation(){
+			return `calc(100% - ${this.bottomNavigationHeight}px)`;
+		}
+	}
 }
 </script>
 
 <style>
+html, body, #__nuxt, #__layout{
+	width: 100%;
+	height: 100%;
+}
+
 html{
 	overflow: hidden;
+}
+body{
+	height: 100%;
+	position: fixed;
+	overflow-y: hidden;
 }
 </style>
--- nuxt.config.js
+++ nuxt.config.js
@@ -12,7 +12,6 @@ export default {
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: '' },
-      { name: 'format-detection', content: 'telephone=no' }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
