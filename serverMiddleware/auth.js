69286192e2acff70a1eb34cc567c29483020e1f9
ItsOKayCZ <DjSlimeLP@gmail.com>
2021-12-10 18:51:03 +0100

    Add cookie

--- serverMiddleware/auth.js
+++ serverMiddleware/auth.js
@@ -8,11 +8,6 @@ const { authPath, authCookie } = process.env;
 
 app.use(cookieParser());
 app.use(function(req, res, next){
-	if(process.env.NODE_ENV == 'development'){
-		next();
-		return;
-	}
-
 	if(req.url ==  `/${authPath}`){
 		res.cookie('authCookie', authCookie, { maxAge: 1000 * 60 * 60 * 24 * 365 * 10, sameSite: "lax" });
 		console.log(`Sent cookie for ${req.ip}`);
