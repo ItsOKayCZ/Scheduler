fadd79256bb56fa9c5d79351422e4e3f31c864af
ItsOKayCZ <DjSlimeLP@gmail.com>
2021-12-26 18:50:29 +0100

    Renamed

--- plugins/mixin.js
+++
@@ -1,9 +0,0 @@
-import Color from './mixins/Color';
-import Events from './mixins/Events';
-import Categories from './mixins/Categories';
-
-export default {
-	...Categories,
-	...Events,
-	...Color,
-};
